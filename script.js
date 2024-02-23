73e3f4dcdb2b9316c09f5f0b8212bc3723f67953
Illabeklemisev <160260282+Illabeklemisev@users.noreply.github.com>
2024-02-23 16:33:15 +0000

    Додали малювання кола

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function draw(event) {
     let canvas = document.getElementById("imgCanvas");
+    let ctx = canvas.getContext("2d");
+    let rect = canvas.getBoundingClientRect();
+
+    let posX = event.clientX;
+    let pasY = event.clientY;
+
+    ctx.fillstyle = "#00ff00"
+
+    ctx.arc(posX, posY, 50, 0, 2 * Math.PI)
+
+    
 }
